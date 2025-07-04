6ad94c71d12a8fc11c28ff185f43e64a8df8646d
Phil Kipkemboi <kipkemboiphil@gmail.com>
2025-07-04 14:46:47 +0300

    Add a new doctype

---
+++ clinical/clinical/doctype/test_doctype/test_doctype.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Phil Kipkemboi and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Test Doctype", {
+// 	refresh(frm) {
+
+// 	},
+// });
